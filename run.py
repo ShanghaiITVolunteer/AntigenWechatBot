bd6e4cbcbeeefe6fe5e70c98be5c1a4c3587dc9c
骑马小猫 <1435130236@qq.com>
2022-04-19 22:37:49 +0800

    update the message forwarder

--- run.py
+++ run.py
@@ -1,28 +1,21 @@
 import asyncio
 import os
-from wechaty_puppet import get_logger
 
-from wechaty import Wechaty, RoomInvitation, WechatyOptions
+from wechaty import Wechaty, WechatyOptions
 from antigen_bot.plugins import (
     MessageForwarderPlugin,
     WatchRoomTopicPlugin,
-    InfoDownloaderPlugin
 )
 
-class WechatyBot(Wechaty):
-
-    async def on_room_invite(self, room_invitation: RoomInvitation) -> None:
-        a = ''
 
 if __name__ == "__main__":
     os.environ['WECHATY_LOG'] = 'silly'
     options = WechatyOptions(
         port=5003
     )
-    bot = WechatyBot(options)
+    bot = Wechaty(options)
     bot.use([
         MessageForwarderPlugin(),
         WatchRoomTopicPlugin(),
-        InfoDownloaderPlugin()
     ])
     asyncio.run(bot.start())
