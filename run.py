ed42579e6bbcede67033fed316a2d545e4c34dab
bigbrother666 <zeming.zhao@gmail.com>
2022-04-24 14:12:36 +0800

    initial on-call

--- run.py
+++ run.py
@@ -1,22 +1,22 @@
 import asyncio
 from wechaty import Wechaty, WechatyOptions, WechatyPluginOptions
 from antigen_bot.plugins import (
-    MessageForwarderPlugin,
+    MessageForwarderPlugin, OnCallNoticePlugin
 )
 
 
 if __name__ == "__main__":
     options = WechatyOptions(
-        port=5003
+        port=5005
     )
     bot = Wechaty(options)
     bot.use([
         MessageForwarderPlugin(
             config_file='.wechaty/message_forwarder_v2.json'
         ),
-        MessageForwarderPlugin(
-            options=WechatyPluginOptions(name='MessageForwarderTestPlugin'),
-            config_file='.wechaty/message_forwarder_test.json'
+        OnCallNoticePlugin(
+            options=WechatyPluginOptions(name='Jiayioncalltest'),
+            config_file='.wechaty/qun_forwarder.json'
         )
     ])
     asyncio.run(bot.start())
