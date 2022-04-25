b928ea27c2dc0027286749303df50c080d929e61
bigbrother666 <zeming.zhao@gmail.com>
2022-04-25 23:11:35 +0800

    resolve conflict

--- run.py
+++ run.py
@@ -1,18 +1,35 @@
 import asyncio
+import os
+import sys
+
 from wechaty import Wechaty, WechatyOptions, WechatyPluginOptions
+
+from dotenv import load_dotenv
+
 from antigen_bot.plugins import (
     MessageForwarderPlugin, OnCallNoticePlugin
 )
+from antigen_bot.plugins.conv2convs import Conv2ConvsPlugin
+from antigen_bot.plugins.health_check import HealthCheckPlugin, HealthCheckPluginOptions
+from antigen_bot.plugins.dynamic_code import DynamicCodePlugin
 
+
+async def final_failure_handler(*args, **kwargs):
+    sys.exit()
 
 if __name__ == "__main__":
+    load_dotenv()
     options = WechatyOptions(
-        port=int(os.environ.get('PORT', 8004))
+        port=int(os.environ.get('PORT', 8004)),
     )
     bot = Wechaty(options)
     bot.use([
         MessageForwarderPlugin(
             config_file='.wechaty/message_forwarder_v2.json'
+        ),
+        MessageForwarderPlugin(
+            options=WechatyPluginOptions(name='MessageForwarderTestPlugin'),
+            config_file='.wechaty/message_forwarder_test.json'
         ),
         OnCallNoticePlugin(
             config_file='.wechaty/on_call_notice.json'
